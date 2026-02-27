be3270c4717f1efe7765778b5c46108335852b76
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-27 21:20:11 +0530

    Checked multiple spaces testcase

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -4,12 +4,15 @@ public class ReverseWords {
                 Scanner sc = new Scanner(System.in);
                 String s = sc.nextLine();
                 String[] words = s.split(" ");
+                System.out.println(words.length);
                 for(int i = words.length - 1; i >= 0; i--){
-                        if(i!=0){
-                                System.out.print(words[i] + " ");
-                        }
-                        else{
-                                System.out.print(words[i]);
+                        if(words[i]!=""){
+                                if(i!=0){
+                                        System.out.print(words[i] + " ");
+                                }
+                                else{
+                                        System.out.print(words[i]);
+                                }
                         }
                 }
         }
